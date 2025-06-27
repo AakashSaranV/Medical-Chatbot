e644ce51dd16286cf2a3d63f4cbf26b62623e735
AakashSaranV <aakashmathes@gmail.com>
2025-06-27 12:18:21 +0530

    folder created

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Medical-ChatBot',
+    version='0.1.0',
+    author='Aakash Saran V',
+    author_email='aakaahmathes@gamil.com',
+    packages=find_packages(),
+    install_requires=[])
